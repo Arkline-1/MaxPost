eeec6b82af21d526eab5bc92ddefd991cd54db2a
Arkline <mikegamesaccount@mail.ru>
2026-04-02 22:33:47 +0300

    Change setup.py and tg_bot.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 MAX_TOKEN = os.getenv("MAX_TOKEN")
 MAX_CHAT_ID = int(os.getenv("MAX_CHAT_ID"))
-MAX_API_URL = "https://platform-api.max.ru"
 
 TG_TOKEN = os.getenv("TG_TOKEN")
 TG_CHAT_ID = int(os.getenv("TG_CHAT_ID"))
--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,4 @@
 from aiogram import Bot
-from aiogram.types import FSInputFile
 from setup import TG_CHAT_ID
 
 
@@ -7,9 +6,9 @@ async def send_text(bot: Bot, text: str) -> None:
     await bot.send_message(chat_id=TG_CHAT_ID, text=text)
 
 
-async def send_photo(bot: Bot, photo_path: str) -> None:
-    await bot.send_photo(chat_id=TG_CHAT_ID, photo=FSInputFile(photo_path))
+async def send_photo(bot: Bot, photo_url: str) -> None:
+    await bot.send_photo(chat_id=TG_CHAT_ID, photo=photo_url)
 
 
-async def send_document(bot: Bot, file_path: str) -> None:
-    await bot.send_document(chat_id=TG_CHAT_ID, document=FSInputFile(file_path))
+async def send_document(bot: Bot, file_url: str) -> None:
+    await bot.send_document(chat_id=TG_CHAT_ID, document=file_url)
