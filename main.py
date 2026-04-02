7cef6fed4e196c25925c38dc7a094900def71957
mr-far <88129219+mr-far@users.noreply.github.com>
2026-04-03 00:46:18 +0300

    Impl task system

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import asyncio
+from datetime import datetime as dt
+
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from setup import TG_TOKEN
-from websocket import MaxClient
+
+from setup import TG_TOKEN, MAX_CHAT_ID
+from ws import MaxClient, MaxEvent, GetChatMessagesResponse, Opcode, create_fetch_chat_messages_obj
+
+
+async def get_messages(client: MaxClient):
+    d = create_fetch_chat_messages_obj(
+        MAX_CHAT_ID, dt.now().timestamp() * 1000, before=10)
+
+    await client.send(d)
+
+
+async def on_message(event: MaxEvent, injection: dict):
+    pass
 
 
 async def main():
@@ -13,6 +27,9 @@ async def main():
     )
 
     client = MaxClient({"bot": bot})
+
+    client.add_loop_task(300, get_messages)
+    client.add_listener(Opcode.GET_CHAT_MESSAGES, on_message)
 
     await client.run()
 
