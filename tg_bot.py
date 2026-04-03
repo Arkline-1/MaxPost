44add58d530eddc676d45c2ea0ecc37cd071ea87
Arkline <mikegamesaccount@mail.ru>
2026-04-03 09:30:46 +0300

    Update tg_bot.py - new function to send all types of messages

--- tg_bot.py
+++ tg_bot.py
@@ -2,13 +2,11 @@ from aiogram import Bot
 from setup import TG_CHAT_ID
 
 
-async def send_text(bot: Bot, text: str) -> None:
-    await bot.send_message(chat_id=TG_CHAT_ID, text=text)
-
-
-async def send_photo(bot: Bot, photo_url: str) -> None:
-    await bot.send_photo(chat_id=TG_CHAT_ID, photo=photo_url)
-
-
-async def send_document(bot: Bot, file_url: str) -> None:
-    await bot.send_document(chat_id=TG_CHAT_ID, document=file_url)
+async def send_to_telegram(bot: Bot, data_list: list[dict]):
+    for item in data_list:
+        caption = f"[{item['sender']}] - ({item['time']})\n\n{item['text']}"
+        
+        if not item['attaches']:
+            await bot.send_message(TG_CHAT_ID, caption)
+        else:
+            await bot.send_photo(TG_CHAT_ID, photo=item['attaches'][0], caption=caption)
