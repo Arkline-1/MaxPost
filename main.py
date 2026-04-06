c205342a3e8d362a5c40423fee95130fc6efa69c
mr-far <88129219+mr-far@users.noreply.github.com>
2026-04-06 19:45:01 +0300

    Update handler

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
 import asyncio
 from aiogram import Bot
-from setup import TG_TOKEN, MAX_CHAT_ID
-from ws import MaxClient, Opcode, create_fetch_chat_messages_obj, MaxEvent, GetChatMessagesResponse, Message
-from tg_bot import send_to_telegram
+from setup import TG_TOKEN
+from ws import MaxClient, Opcode, MaxEvent, GetChatMessagesResponse, Message, NewMessage
+from telegram import forward_message_to_telegram
 
 
-async def universal_handler(event: MaxEvent, injection: dict | None):
-    bot: Bot = injection.get("bot")
+async def universal_handler(event: MaxEvent, bot: Bot):
+    messages: list[Message] = list()
 
-    raw_messages = []
-
-    if event.opcode == Opcode.GET_CHAT_MESSAGES:
-        raw_messages = event.payload.get("messages", [])
+    if event.opcode == Opcode.GET_CHAT_HISTORY:
+        messages = GetChatMessagesResponse(**event.payload).messages
 
     elif event.opcode == Opcode.MESSAGE_CREATE:
-        single_msg = event.payload.get("message")
-        if single_msg:
-            raw_messages = [single_msg]
+        single_msg = NewMessage(**event.payload).message
 
-    if not raw_messages:
+        if single_msg:
+            messages = [single_msg]
+
+    if not messages:
         return
 
-    processed = []
-    for m_data in raw_messages:
-        msg = Message(**m_data)
-
+    for msg in messages:
         links = [d.get("baseUrl") for d in msg.attaches]
+        attaches = [link for link in links if link]
 
-        processed.append({
-            "sender": msg.sender,
-            "time": msg.time,
-            "text": msg.text,
-            "attaches": [link for link in links if link]
-        })
-
-    await send_to_telegram(bot, processed)
+        await forward_message_to_telegram(bot, msg.time, msg.sender, msg.text, attaches)
 
 
 async def main():
     bot = Bot(token=TG_TOKEN)
-    client = MaxClient(injection={"bot": bot})
+    client = MaxClient(bot=bot)
 
-    client.add_listener(Opcode.GET_CHAT_MESSAGES, universal_handler)
+    client.add_listener(Opcode.GET_CHAT_HISTORY, universal_handler)
     client.add_listener(Opcode.MESSAGE_CREATE, universal_handler)
 
     await client.run()
